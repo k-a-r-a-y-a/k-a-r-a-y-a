6b270c322b721a6ba6e90b572492644164a5fe6f
Dennis Karaya <karayadennis04@gmail.com>
2026-05-26 09:43:23 +0300

    add health endpoint

--- api.py
+++ api.py
@@ -1,1 +1,1 @@
-def get_users():\n    return [{'id': 1, 'name': 'John'}]\n\ndef get_orders():\n    return [{'id': 1, 'total': 100}]
+def get_users():\n    return [{'id': 1, 'name': 'John'}]\n\ndef get_orders():\n    return [{'id': 1, 'total': 100}]\n\ndef health():\n    return 'ok'
